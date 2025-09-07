42a63d013b20f8c563c63ca58deb10b1644ca347
Tomás <tomivenitez.14@gmail.com>
2025-09-07 20:28:06 -0300

    Feat: add repositorioPaciente and spec.

---
+++ src/errors/paciente.js
@@ -0,0 +1,4 @@
+const ERROR_PACIENTE_NO_ENCONTRADO = 'Paciente no encontrado';
+const ERROR_PACIENTE_CREACION = 'Error al crear el paciente';
+
+export { ERROR_PACIENTE_NO_ENCONTRADO, ERROR_PACIENTE_CREACION };
