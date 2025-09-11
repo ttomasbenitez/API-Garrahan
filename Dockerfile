cd58f06febb655d9d86953b78d2200917f6717b2
Carolina Mauro <carolina@macBookPro.local>
2025-09-11 02:26:09 -0300

    corre servidor

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY package*.json ./
 RUN npm install --only=production
 
 COPY . .
-CMD ["node", "src/app.js"]
+CMD ["node", "src/server.js"]
--- src/db/db_init/init_entrypoint.sh
+++ src/db/db_init/init_entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 echo "Esperando a que Oracle esté listo..."
 
 # Espera hasta que Oracle acepte conexiones usando sqlplus
-until echo "exit" | sqlplus -L "system/app_pass@oracle:1521/XE" > /dev/null 2>&1; do
+until echo "exit" | sqlplus -L "system/oracle@oracle:1521/XE" > /dev/null 2>&1; do
   echo "Esperando a Oracle..."
   sleep 10
 done
 
 echo "Creando app_user y otorgando permisos..."
-sqlplus -s system/app_pass@//oracle:1521/XEPDB1 @/init/01_create_app_user.sql
+sqlplus -s system/oracle@//oracle:1521/XEPDB1 @/init/01_create_app_user.sql
 
 touch /tmp/init-ok
 tail -f /dev/null
