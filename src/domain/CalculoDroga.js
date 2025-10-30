4688318071800a36465c6bbf7dfe227d916f9ea2
Tomás <tomivenitez.14@gmail.com>
2025-10-30 20:11:55 -0300

    Refactor: fix calculo droga to use peso and use function to calculate superficie corporal.

--- src/domain/CalculoDroga.js
+++ src/domain/CalculoDroga.js
@@ -1,12 +1,11 @@
 export default class CalculoDroga {
-  constructor({ fuerza_valor_requerida, fuerza_unidad_requerida, cantidad_dias, frecuencia_diaria, imc, nueva_fuerza_valor, nueva_fuerza_unidad }) {
+  constructor({ fuerza_valor_requerida, fuerza_unidad_requerida, cantidad_dias, frecuencia_diaria, peso, nueva_fuerza_valor, nueva_fuerza_unidad }) {
 
     const normalizarAMiligramos = (valor, unidad) => {
-      const valorNumerico = Number(valor);
+      const valorNumerico = valor;
       if (isNaN(valorNumerico)) return 0;
 
       if (unidad && typeof unidad === 'string' && unidad.toLowerCase().includes('gr')) {
-        // 1 gramo = 1000 miligramos
         return valorNumerico * 1000;
       }
 
@@ -19,24 +18,28 @@ export default class CalculoDroga {
     // Presentación Elegida: Normalizada a mg por unidad (si la presentación es en "gr")
     this.fuerza_presentacion_mg = normalizarAMiligramos(nueva_fuerza_valor, nueva_fuerza_unidad);
     this.fuerza_unidad = 'mg';
-    this.cantidad_dias = Number(cantidad_dias);
-    this.frecuencia_diaria = Number(frecuencia_diaria);
-    this.sc = Number(imc);
+    this.cantidad_dias = cantidad_dias;
+    this.frecuencia_diaria = frecuencia_diaria;
+    this.peso = peso;
+  }
+
+  calcularSuperficieCorporalPediatrica() {
+    return (this.peso * 4 + 7) / (this.peso + 90);
   }
 
   getCantidadBase() {
     const cantidadBase = this.dosis_requerida_mg_m2 * this.cantidad_dias * this.frecuencia_diaria;
-    return Number(cantidadBase.toFixed(2));
+    return cantidadBase.toFixed(2);
   }
 
   getCantidadTotal() {
-    const cantidadTotal = this.dosis_requerida_mg_m2 * this.cantidad_dias * this.frecuencia_diaria * this.sc;
-    return Number(cantidadTotal.toFixed(2));
+    const cantidadTotal = this.dosis_requerida_mg_m2 * this.cantidad_dias * this.frecuencia_diaria * this.calcularSuperficieCorporalPediatrica();
+    return cantidadTotal.toFixed(2);
   }
 
   getUnidades() {
     if (!this.fuerza_presentacion_mg || this.fuerza_presentacion_mg === 0) return 0;
-    const cantidadTotalNumerica = this.dosis_requerida_mg_m2 * this.cantidad_dias * this.frecuencia_diaria * this.sc;
+    const cantidadTotalNumerica = this.dosis_requerida_mg_m2 * this.cantidad_dias * this.frecuencia_diaria * this.calcularSuperficieCorporalPediatrica();
 
     return Math.ceil(cantidadTotalNumerica / this.fuerza_presentacion_mg);
   }
